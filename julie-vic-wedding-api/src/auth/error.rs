fd7586a5971e954132b791d69e7ba82778b96fba
Victor Quiroz Castro <victorhqc@gmail.com>
2020-02-26 12:30:55 -0600

    style: remove dead code

--- julie-vic-wedding-api/src/auth/error.rs
+++ julie-vic-wedding-api/src/auth/error.rs
@@ -2,7 +2,6 @@ use http::status::StatusCode;
 use oauth2::basic::BasicErrorResponseType;
 use oauth2::RequestTokenError;
 use reqwest::Error as ReqwestError;
-// use serde_json::error::Error as SerdeJsonError;
 use std::convert::From;
 use thiserror::Error as ThisError;
 use url::ParseError;
@@ -16,16 +15,9 @@ pub enum AuthError {
     #[error("Malformed Redirect Url")]
     BadRedirectUrl(ParseError),
     #[error("Can't parse token `{}`", _0)]
-    // ParseTokenError(SerdeJsonError),
-    // #[error("Can't exchange token `{}`", _0)]
     ExchangeTokenError(RequestTokenError<BasicErrorResponseType>),
-    // #[error("Failed to request token `{}`", _0)]
-    // OAuth2ErrorResponse(ErrorResponse<BasicErrorResponseType>),
     #[error("Can't make HTTP Request `{}`", _0)]
     ReqwestError(ReqwestError),
-
-    #[error("Something went wrong `{}`", _0)]
-    Other(String),
 }
 
 impl WithCode for AuthError {
@@ -34,11 +26,8 @@ impl WithCode for AuthError {
 
         match self {
             BadRedirectUrl(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            // ParseTokenError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             ExchangeTokenError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            // OAuth2ErrorResponse(_) => StatusCode::INTERNAL_SERVER_ERROR,
             ReqwestError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            Other(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
