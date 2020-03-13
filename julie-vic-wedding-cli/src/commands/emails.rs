78c5578dd3409d730d9835da49b22e0d86c9ef8c
Victor Quiroz Castro <victorhqc@gmail.com>
2020-03-13 16:56:15 +0100

    feat: wip html emails

--- julie-vic-wedding-cli/src/commands/emails.rs
+++ julie-vic-wedding-cli/src/commands/emails.rs
@@ -3,6 +3,8 @@ use diesel::PgConnection;
 use julie_vic_wedding_core::attend_status_type::AttendStatus;
 use std::convert::From;
 use std::env;
+use std::fs;
+use std::path::Path;
 use uuid::Uuid;
 
 pub fn send_rsvp_emails(conn: PgConnection) {
@@ -29,12 +31,15 @@ pub fn send_rsvp_emails(conn: PgConnection) {
 
         let full_name = format!("{} {}", user.1, last_name);
         let user_email = user.0.clone();
+        let sender_email = format!("boda@{}", domain);
+        let subject = String::from("Invitación");
+
+        let template_path = Path::new("./templates/invitation-en.html");
+        let template = fs::read_to_string(template_path).expect("Couldn't read e-mail template");
+
         println!("Will send e-mail for {} ({})", full_name, user_email);
 
-        let sender_email = format!("rsvp@{}", domain);
-        println!("sender: {}", sender_email);
-
-        let email = Email::new(sender_email, user_email, full_name);
+        let email = Email::new(sender_email, user_email, full_name, subject, template);
 
         match send_email(&email) {
             Ok(_) => println!("Email to {} was successfully sent", email.to),
@@ -52,8 +57,8 @@ fn send_email(email: &Email) -> Result<(), Box<dyn std::error::Error>> {
     let form = reqwest::blocking::multipart::Form::new()
         .text("from", email.from.clone())
         .text("to", email.to.clone())
-        .text("subject", String::from("Test"))
-        .text("text", String::from("Hello World"));
+        .text("subject", String::from("Invitation"))
+        .text("html", email.html.clone());
 
     let client = reqwest::blocking::Client::new();
 
@@ -70,13 +75,23 @@ fn send_email(email: &Email) -> Result<(), Box<dyn std::error::Error>> {
 struct Email {
     pub from: String,
     pub to: String,
+    pub subject: String,
+    pub html: String,
 }
 
 impl Email {
-    pub fn new(from_email: String, to_email: String, to_name: String) -> Self {
+    pub fn new(
+        from_email: String,
+        to_email: String,
+        to_name: String,
+        subject: String,
+        html: String,
+    ) -> Self {
         Email {
             from: from_email,
             to: format!("{} <{}>", to_name, to_email),
+            subject,
+            html,
         }
     }
 }
